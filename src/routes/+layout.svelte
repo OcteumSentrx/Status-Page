735eb1411f243c48e2da65f108d3645f8e75ff35
OcteumSentrx <sxddnsa@gmail.com>
2026-07-20 21:53:45 -0400

    Update +layout.svelte
    Fixed a missing closing URL tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 <footer class="footer footer-center bg-base-300 text-base-content p-4 sticky top-[100vh]">
 	<aside>
 		<p>
-			Status for 👾<a href="https://sentrx.octeum.com">Octeum Sentrx powered by <a href="https://github.com/Bullrich/GitHub-Status-Page">GitHub Status Page</a>
+			Status for 👾<a href="https://sentrx.octeum.com">Octeum Sentrx </a> powered by <a href="https://github.com/Bullrich/GitHub-Status-Page">GitHub Status Page</a>
 		</p>
 	</aside>
 </footer>
